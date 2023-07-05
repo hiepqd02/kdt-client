a53fcc0e2fef7a1f898c465920e4fcdb028d6568
HiepTD <hiepqd02@gmail.com>
2023-07-05 10:57:55 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useState } from 'react'
+import axios from 'axios'
+export default function App() {
+  const [words, setWords] = useState('')
+  const [response, setResponse] = useState('')
 
-function App() {
+  const handleSubmit = (e) => {
+    e.preventDefault()
+
+    axios
+      .post('http://localhost:8080/chat', { words })
+      .then((res) => {
+        setResponse(res.data)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+
+    setWords('')
+  }
+
+  const handlePrompt = (e) => {
+    setWords(e.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container container-sm p-1">
+      {' '}
+      <form className="form" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="">Input words</label>
+          <input
+            className="shadow-sm"
+            type="text"
+            placeholder="Enter text"
+            value={words}
+            onChange={handlePrompt}
+          />
+        </div>{' '}
+        <button className="btn btn-accept w-100" type="submit">
+          Go
+        </button>
+      </form>
+      <div className="bg-darkGreen  mt-2 p-1 border-5">
+        <p className="text-light">{response ? response : 'Wait...'}</p>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
